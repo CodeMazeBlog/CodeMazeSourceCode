4fa10d3aa7730e27032a593d828fd770089d1065
Muhammed Saleem <muhammedsalimp@gmail.com>
2019-04-10 13:47:12 +0530

    Part5: Routing and file upload

---
+++ MVCSeries/Part5/WorkingWithRouting/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkingWithRouting.Controllers
+{
+    public class BooksController : Controller
+    {     
+        [Route("Home")]
+        [Route("Home/Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Home/Details/{id:int}")]
+        public IActionResult Details(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+    }
+}
