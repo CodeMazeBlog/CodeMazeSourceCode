e67cbe51ed7f9fe13c3b96748b4bb34f2c0e7804
Muhammed Saleem <muhammedsalimp@gmail.com>
2019-06-27 18:04:42 +0530

    Final commit. All changes

---
+++ MVCSeries/Part7/WorkingWithFilters/Controllers/BooksController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WorkingWithFilters.Filters;
+using WorkingWithFilters.Models;
+
+namespace WorkingWithFilters.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [ValidateModel]
+        [HttpPost]
+        public IActionResult Create([FromBody]Book book)
+        {
+            return View();
+        }
+
+    }
+}
