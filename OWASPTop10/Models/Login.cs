d029bdafc4fbc499534ffca99fb1d615c2c455e6
Muhammed Saleem P <muhammedsalimp@gmail.com>
2020-01-15 11:35:12 +0530

    OWASP Top 10 - Injection
    OWASP Top 10 - Injection

---
+++ OWASPTop10/Models/Login.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OWASPTop10.Models
+{
+    public class Login
+    {
+        public int ID { get; set; }
+
+        public string Username { get; set; }
+
+        [ShouldNotContainSingleQuotesValidation(ErrorMessage = "Cannot contain single quotes")]
+        public string Password { get; set; }
+
+        public string Message { get; set; }
+    }
+
+    public sealed class ShouldNotContainSingleQuotesValidation : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            return !value.ToString().Contains("'");
+        }
+    }
+}
